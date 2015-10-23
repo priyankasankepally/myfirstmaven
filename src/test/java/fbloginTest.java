b064df5cd78ca7fb5d76bdcf59cca49da7b48eab
Ajay <ajayreddy.402@gmail.com>
2015-10-23 15:03:34 -0700

    my foutth commit

--- src/test/java/fbloginTest.java
+++ src/test/java/fbloginTest.java
@@ -1,25 +1,31 @@
+
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import Errorcollector.ErrorCollector;
+
 
 public class fbloginTest {
 
-		
+		//public static Errorcollector.ErrorCollector ec = new Errorcollector.ErrorCollector();
 		@Test
-		public void dologin(){
+		public static void dologin(){
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.facebook.com");
 		System.out.println(driver.getTitle());
-		String Actual = "facebook";
+		String Actual = "Google";
 		try{
 Assert.assertEquals(Actual, driver.getTitle());
 		}
 		catch(Throwable t){
-			
-			System.out.println(t.getMessage());
+
+		ErrorCollector.addVerificationFailure(t);
+		
 		}
+		
 
 		}
 
